416da7ef507d7506b3429380a5d7f8ae27cfaa0e
gstrainovic <g.strainovic@gmail.com>
2024-10-16 14:54:17 +0200

    add ui

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,22 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use evdev::{Device, InputEventKind};
 use std::io::Result;
-fn main() -> Result<()> {
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+pub fn looper() -> Result<()> {
     let device_name = "Lixin BCST-60 Keyboard";
 
     // Durchsuche die verfügbaren Eingabegeräte aus /dev/input/event*
